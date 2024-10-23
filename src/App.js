0031d004306efe9792c576128d9d1fad78735ff0
kishan1004 <146201840+kishan1004@users.noreply.github.com>
2024-10-23 12:10:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import "./index.css";
+import Topbar from "./components/Topbar.js";
+import ProductPage from "./components/ProductPage.js";
+
+function App() {
+  return (
+    <div className="App font-beatrice bg-gray-100">
+      <Topbar />
+      <ProductPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        beatrice: ['"Beatrice Deck"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
